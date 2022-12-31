1f5a065ff5e5e194757b233fe7f52c1cabc1bef3
kobi krasnoff <krasnoff.kobi@gmail.com>
2022-12-31 13:09:10 +0200

    first Commit

---
+++ src/components/chatGPTComponent/chatGPTComponent.tsx
@@ -0,0 +1,11 @@
+import styles from './ChatGPTComponent.module.scss';
+
+function ChatGPTComponent() {
+  return (
+    <div className={styles.wrapper}>
+      ChatGPTComponent
+    </div>
+  );
+}
+
+export default ChatGPTComponent;
