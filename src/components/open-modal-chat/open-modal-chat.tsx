2960384988cccda4291d36da64e515547d05640c
kobi krasnoff <krasnoff.kobi@gmail.com>
2023-01-04 22:50:20 +0200

    start implement useSpeechRecognition

--- src/components/open-modal-chat/open-modal-chat.tsx
+++ src/components/open-modal-chat/open-modal-chat.tsx
@@ -2,12 +2,22 @@ import { useState } from 'react';
 import styles from './open-modal-chat.module.scss';
 import Modal from 'react-bootstrap/Modal';
 import { Button } from 'react-bootstrap';
+import { useSpeechRecognition } from 'react-speech-kit';
+
 
 function OpenModalChatboxComponent() {
     const [show, setShow] = useState(false);
+    const [value, setValue] = useState('');
+    const { listen, listening, stop } = useSpeechRecognition({
+        onResult: (result: any) => {
+          setValue(result);
+        },
+    });
     
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+
 
     return (
         <>
@@ -24,7 +34,11 @@ function OpenModalChatboxComponent() {
                 <Modal.Header closeButton>
                 <Modal.Title>Modal heading</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>
+                    <Button variant="danger">
+                        Record
+                    </Button>
+                </Modal.Body>
                 <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
                     Close
